a3027a13838b8f700ae34609e501ab7a5c20a053
gmurad97 <murad.dev@bk.ru>
2023-08-05 05:51:27 +0400

    fixed vertical slider

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-$(function () {
-    $('.owl-carousel').owlCarousel({
-        items: 1,
-        loop: true,
-        nav: false,
-        autoplay: true
-    });
-    // this code below enables drag and drop vertically. Unfortunately I was unable to disable horizontal drag and drop so it will remain active, but we already have something now =D
-    $('.owl-carousel').data('owl.carousel').difference = function (first, second) {
-        return {
-            x: first.x - second.x + (first.y - second.y),
-            y: first.y - second.y
-        };
-    };
-});
+const slider = document.querySelector(".slider");
+const slideImages = document.querySelectorAll(".slider__img");
+const slideImagesCount = slideImages.length;
+const slideInterval = 2000;
+let slideIndex = 0;
+let autoSlideInterval;
+
+slider.appendChild(slideImages[0].cloneNode(true));
+
+function startAutoSlide() {
+  clearInterval(autoSlideInterval);
+  autoSlideInterval = setInterval(() => {
+    slideIndex = (slideIndex + 1) % slideImagesCount;
+    slider.style.transform = `translateY(-${slideIndex * 100}vh)`;
+  }, slideInterval);
+}
+
+startAutoSlide();
